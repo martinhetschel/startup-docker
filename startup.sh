a529f3bf2b9ce35611d7eeb8e4a39e5995e8ac71
martinhetschel <martin.hetschel@mach1kart.de>
2020-02-23 17:48:13 +0100

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,24 @@
+# portainer
+cd ~
+docker volume create portainer_data
+docker stop portainer
+docker rm portainer
+docker run -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --restart=unless-stopped --name=portainer portainer/portainer
+
+# Mosquitto
+cd ~
+git clone https://github.com/AM2H-Development/mqtt.git
+cd mqtt
+docker-compose up -d
+
+# Influx
+cd ~
+git clone https://github.com/AM2H-Development/influx.git
+cd influx
+docker-compose up -d
+
+# Theia und Nginx Reerse-Proxy
+cd ~
+git clone https://github.com/AM2H-Development/theia.git
+cd theia
+docker-compose up -d
