8cd56c13bce26fd5be56680ad55b10a5b9702a92
martinhetschel <martin.hetschel@mach1kart.de>
2021-10-23 13:02:05 +0200

    Update startup.sh
    Updated Portainer to CE

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ cd ~
 docker volume create portainer_data
 docker stop portainer
 docker rm portainer
-docker run -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --restart=unless-stopped --name=portainer portainer/portainer
+docker run -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --restart=unless-stopped --name=portainer portainer/portainer-ce
 
 # Mosquitto
 cd ~
